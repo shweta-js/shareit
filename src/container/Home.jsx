cb8a3d2eb6e62b7de160c1199683d2308880fa77
shweta-js <shwetajs200@gmail.com>
2022-05-03 23:25:40 +0530

    implemented story component

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js'
 import Posts from '../components/Posts.js'
+import Stories from "../components/Stories.js"
+import "./Home.css"
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <div className="home-1">
+            <Stories/>
             <Posts/>
+            </div>
+            
         </div>
     )
 }
